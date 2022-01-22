30d573c80626eb91c08ad43bb602260c4051651e
Sharvin1106 <sharvin.kogilavanan@gmail.com>
2022-01-23 03:07:32 +0800

    Added endpoint to create users and location related things

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import pandas as pd
 from sklearn.metrics.cluster import normalized_mutual_info_score
 from sklearn.metrics.cluster import adjusted_rand_score
 from itertools import combinations
+import uuid
 
 app = Flask(__name__)
 
@@ -41,6 +42,88 @@ def get_centrality():
         return resp
     else:
         return not_found()
+
+
+@app.route("/createUser", methods=['POST'])
+def add_user():
+    _json = request.json
+    _uid = _json['uid']
+
+    if _uid and request.method == 'POST':
+        id = mongo.db.user.insert_one({'uid': _uid, 'uuid': str(uuid.uuid4())})
+        resp = jsonify("User added successfully")
+        resp.status_code = 200
+        return resp
+    else:
+        not_found()
+
+
+@app.route("/uploadContactDetails", methods=['PATCH'])
+def upload_details():
+    _json = request.json
+    _uuid = _json['uuid']
+    _closeContact = _json['closeContact']
+    _locationVisited = _json['locationVisited']
+
+    if _uuid and _closeContact and _locationVisited and request.method == 'PATCH':
+        id = mongo.db.user.find_one_and_update({'uuid': _uuid}, {
+                                               "$set": {"closeContact": _closeContact, "locationVisited": _locationVisited}})
+        resp = jsonify("User added successfully")
+        resp.status_code = 200
+        return resp
+    else:
+        not_found()
+
+
+@app.route("/createLocation", methods=['POST'])
+def create_location():
+    _json = request.json
+    _locationName = _json['locationName']
+    _capacity = _json['capacity']
+
+    if _locationName and _capacity and request.method == 'POST':
+        id = mongo.db.location.insert_one(
+            {'locationName': _locationName, 'capacity': _capacity, 'visitorsCount': 0})
+        resp = jsonify("Location created")
+        resp.status_code = 200
+        return resp
+    else:
+        not_found()
+
+
+@app.route("/getLocations", methods=['GET'])
+def get_locations():
+    locations = mongo.db.location.find()
+    resp = dumps(locations)
+    return resp
+
+
+@app.route("/checkIn", methods=['POST'])
+def check_in():
+    _json = request.json
+    _location = _json['location']
+    if _location and request.method == 'POST':
+        id = mongo.db.location.find_one_and_update(
+            {'_id': ObjectId(_location)}, {"$inc": {"visitorsCount": 1}})
+        resp = jsonify("Checked in to location")
+        resp.status_code = 200
+        return resp
+    else:
+        not_found()
+
+
+@app.route("/checkOut", methods=['POST'])
+def check_out():
+    _json = request.json
+    _location = _json['location']
+    if _location and request.method == 'POST':
+        id = mongo.db.location.find_one_and_update(
+            {'_id': ObjectId(_location)}, {"$inc": {"visitorsCount": -1}})
+        resp = jsonify("Checked out from location")
+        resp.status_code = 200
+        return resp
+    else:
+        not_found()
 
 
 @app.errorhandler(404)
